1d7b953a19ec2b1577700e2c4544d6ed57489564
Alex Brown <alexanderbrown.info@gmail.com>
2017-02-26 09:50:44 +0100

    corrected indentation

--- src/Container.js
+++ src/Container.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Container extends Component {
-    render() {
-        return (
-            <div className="container"></div>
-        );
-    }
+	render() {
+		return (
+			<div className="container"></div>
+		);
+	}
 }
 
 export default Container;
